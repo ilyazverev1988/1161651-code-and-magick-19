54feb4e3833a45533aa1a23d8f14d6effab2c677
ilyazverev1988 <ilya.verez@mail.ru>
2020-01-31 21:13:34 +0300

    31.01.2020_2

--- js/stat.js
+++ js/stat.js
@@ -11,7 +11,7 @@ var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var BAR_GAP = 50;
 var BASELINE = 250;
-var textMassive = ['Ура вы победили!', 'Список результатов:'];
+var textMassives = ['Ура вы победили!', 'Список результатов:'];
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -20,10 +20,6 @@ var renderCloud = function (ctx, x, y, color) {
 
 var getRandom = function (min, max) {
   return min + Math.floor(Math.random() * (max - min + 1));
-};
-
-var getMaxElement = function (arr) {
-  return Math.max.apply(Math, arr);
 };
 
 var renderText = function (ctx, text, x, y) {
@@ -40,24 +36,37 @@ var renderColumn = function (ctx, left, bottom, columnHeight, color) {
 
 window.renderStatistics = function (ctx, names, times) {
 
-  var maxTime = getMaxElement(times);
-
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  for (var i = 0; i < textMassive.length; i++) {
-    renderText(ctx, textMassive[i], TEXT_X, TEXT_Y + i * TEXT_GAP);
-  }
+  var getTextCongratulate = function () {
+    textMassives.forEach(function (text, index) {
+      renderText(ctx, text, TEXT_X, TEXT_Y + index * TEXT_GAP);
+    });
+  };
 
-  for (i = 0; i < names.length; i++) {
-    var color = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + getRandom(1, 100) + '%, 50%)';
-    var nameX = CLOUD_X + GAP + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i;
-    var nameY = BASELINE + 5;
-    var barHeight = (BAR_HEIGHT * times[i]) / maxTime;
-    var numberY = BASELINE - (BAR_HEIGHT * times[i]) / maxTime - 20;
-    renderText(ctx, names[i], nameX, nameY);
-    renderColumn(ctx, nameX, BASELINE, barHeight, color);
-    renderText(ctx, Math.round(times[i]), nameX, numberY);
-  }
+  var getText = function () {
+    var getMaxElement = function (arr) {
+      return Math.max.apply(Math, arr);
+    };
+    var maxTime = getMaxElement(times);
+    names.forEach(function (name, index) {
+      var time = times[index];
+      var color =
+        name === 'Вы'
+          ? 'rgba(255, 0, 0, 1)'
+          : 'hsl(240, ' + getRandom(1, 100) + '%, 50%)';
+      var nameX = CLOUD_X + GAP + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * index;
+      var nameY = BASELINE + 5;
+      var barHeight = (BAR_HEIGHT * time) / maxTime;
+      var numberY = BASELINE - (BAR_HEIGHT * time) / maxTime - 20;
+      renderText(ctx, name, nameX, nameY);
+      renderColumn(ctx, nameX, BASELINE, barHeight, color);
+      renderText(ctx, Math.round(time), nameX, numberY);
+    });
+  };
+
+  getTextCongratulate();
+  getText();
 };
 
